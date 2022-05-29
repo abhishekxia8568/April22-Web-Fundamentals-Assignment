3d9e0b454d7f2cff48132bcb1339ffceb866727c
Abhishek <abhishekxia8568@gmail.com>
2022-05-29 12:24:29 +0530

    all assignment and test is completed till May

---
+++ May/week 4/shape/shape.js
@@ -0,0 +1,8 @@
+let h=10;
+let b=15;
+if(h===b){
+    console.log('Shape is SQUARE');
+}
+else{
+    console.log('Shape is RECTANGLE');
+}
---
+++ May/week 4/week day/week.js
@@ -0,0 +1,20 @@
+let n=4;
+switch (n) {
+    case 1:console.log('Day is Sunday');
+        break;
+        case 2:console.log('Day is Monday');
+        break;
+        case 3:console.log('Day is Tuesday');
+        break;
+        case 4:console.log('Day is Wednesday');
+        break;
+        case 5:console.log('Day is Thursday');
+        break;
+        case 6:console.log('Day is Friday');
+        break;
+        case 7:console.log('Day is Saturday');
+        break;
+
+    default:console.log('Enter wrong no. of day');
+        break;
+}
